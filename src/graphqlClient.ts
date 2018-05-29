4c6a16389d4a4cd34b2a72714e41dcf884e3da0c
Artem Serga <artem@serga.name>
2018-05-29 22:30:34 +0300

    Temp fix for SSL api url (android)

--- src/graphqlClient.ts
+++ src/graphqlClient.ts
@@ -1,8 +1,12 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
+import { Platform } from "reactxp";
 
-const GRAPHQL_URL = "https://shop.serga.name/graphql";
+
+const GRAPHQL_URL = `${
+  Platform.getType() == "android" ? "http" : "https"
+}://shop.serga.name/graphql`;
 
 const httpLink = createHttpLink({
   uri: GRAPHQL_URL,
