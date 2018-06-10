eff4474a0995431bbac55f88cebe064edf9c8352
Artem Serga <artem@serga.name>
2018-06-10 15:30:49 +0300

    iOS: launch icon set

--- ios/ReactxpShopTests/ReactxpShopTests.m
+++ ios/ReactxpShopTests/ReactxpShopTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RXPTodoListTests : XCTestCase
+@interface ReactxpShopTests : XCTestCase
 
 @end
 
-@implementation RXPTodoListTests
+@implementation ReactxpShopTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
