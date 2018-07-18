d8ad2c5b3e94d72b48e0eb5f6dce0065b9f314b3
Artem Serga <artem@serga.name>
2018-07-18 10:52:41 +0300

    Prettier formatter

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Available options https://prettier.io/docs/en/options.html
+module.exports = {
+    printWidth: 100,
+    parser: "typescript",
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: false,
+    arrowParens: "avoid",
+};
